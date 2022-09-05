984ddc3e938e8b57af92c58b79f3d9adfee57eb5
DrMadWill <nofelsalahov9@gmail.com>
2022-09-05 13:30:13 +0400

    main: Interface

--- Lesson-4 Function/main.js
+++ Lesson-4 Function/main.js
@@ -1,4 +1,16 @@
 function getAverage(a, b, c) {
     return (a + b + (c !== null && c !== void 0 ? c : 0)) / 3;
 }
-console.log(getAverage(1, 2, 3), getAverage(3, 3));
+// console.log(getAverage(1,2,3) , getAverage(3,3))
+var getAverageArr = function () {
+    var a = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        a[_i] = arguments[_i];
+    }
+    var total = 0;
+    a.forEach(function (element) {
+        total += element;
+    });
+    return total / a.length;
+};
+console.log(getAverageArr(1, 2, 34, 55, 5, 67, 8));
---
+++ Lesson-5 Interface/main.ts
@@ -0,0 +1,18 @@
+interface Point{ // Tip kimi
+    x:number;
+    y:number;
+}
+
+interface Passanger{
+    name: string;
+    phone:string;
+}
+
+
+interface Vehicle{ // Sema kimi
+    currentLocation:Point;
+    travelTo(point:Point):void;
+    getDistance(pointA:Point,pointB:Point):number;
+    addPassanger(passanger:Passanger):void;
+    removePassanger(passanger:Passanger):void;
+}
